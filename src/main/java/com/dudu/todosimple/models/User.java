ec7b425b22ace84436ae963d418e6da00b9fc2de
Dudu <eduardo.correa2007@hotmail.com>
2024-07-19 01:08:37 -0300

    Creat Task model

--- src/main/java/com/dudu/todosimple/models/User.java
+++ src/main/java/com/dudu/todosimple/models/User.java
@@ -1,6 +1,7 @@
 package com.dudu.todosimple.models;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Column;
@@ -8,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -50,7 +52,8 @@ public class User {
 	@Size(groups = {CreateUser.class , UpdateUser.class} , min = 8 , max = 60)
 	private String password;
 	
-	//private ArrayList<Task> tasks = new ArrayList<Task>();
+	@OneToMany(mappedBy = "user") //um usuário pode ter várias tasks
+	private List<Task> tasks = new ArrayList<Task>();
 	
 	public User() {
 		
@@ -86,6 +89,15 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	
+	public List<Task> getTasks() {
+		return tasks;
+	}
+	public void setTasks(List<Task> tasks) {
+		this.tasks = tasks;
+	}
+
 	
 	
 	
