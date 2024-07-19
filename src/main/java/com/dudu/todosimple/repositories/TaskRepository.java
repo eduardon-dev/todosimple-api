2c9e1a4409f4a9b39dd7314bc207b20936cb1d55
Dudu <eduardo.correa2007@hotmail.com>
2024-07-19 02:05:14 -0300

    Create Repository

---
+++ src/main/java/com/dudu/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,15 @@
+package com.dudu.todosimple.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dudu.todosimple.models.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task , Long>{
+	
+	List<Task> findByUser_Id(Long id);
+
+}
